31f743d2431e8ce5019df8215bb5e1d783deab38
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2024-02-20 10:42:56 +1100

    sdfsdf

--- TargetingFeatureFlagsEntraID/Controllers/HomeController.cs
+++ TargetingFeatureFlagsEntraID/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using TargetingFeatureFlagsEntraID.Models;
 
 namespace TargetingFeatureFlagsEntraID.Controllers;
 
-[Authorize]
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -16,11 +16,13 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [AllowAnonymous]
     public IActionResult Index()
     {
         return View();
     }
 
+    [AllowAnonymous]
     public IActionResult Privacy()
     {
         return View();
